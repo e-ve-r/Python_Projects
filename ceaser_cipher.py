524e6865bd65788f414079bf3ef4cc41bc678e74
Veer Vikram Singh <19.ever.30@gmail.com>
2025-07-08 15:58:20 +0530

    Add files via upload

---
+++ ceaser_cipher.py
@@ -0,0 +1,52 @@
+print("""           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+""")
+
+def encrypt(text, shift):
+    message = ""
+    for letter in text:
+        for character in alphabet:
+            if character == letter:
+                if ((alphabet.index(character)+shift)>25):
+                    message += alphabet[((alphabet.index(character)+shift)-25)-1]
+                else:
+                    message += alphabet[alphabet.index(character)+shift]
+    
+    return message
+    
+def decrypt(text, shift):
+    message = ""
+    for letter in text:
+        for character in alphabet:
+            if character == letter:
+                if ((alphabet.index(character)-shift)<0):
+                    message += alphabet[((alphabet.index(character)-shift)+25)+1]
+                else:
+                    message += alphabet[alphabet.index(character)-shift]
+    
+    return message
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+if direction == "encode":
+    print(f"Your encrypted message is: {encrypt(text, shift)}\n")
+
+else:
+    print(f"You decrypted message is: {decrypt(text, shift)}\n")
